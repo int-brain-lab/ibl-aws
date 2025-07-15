be6b79b149fa12568cf7b946fc821781a9555524
Olivier Winter <olivier.winter@hotmail.fr>
2025-07-15 14:11:52 +0200

    example to update prefix list for devs with automatic IP fetch

--- examples/update_prefix_list_devs.py
+++ examples/update_prefix_list_devs.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 import iblaws.utils
+import requests
+
+def get_public_ip():
+    response = requests.get('https://api.ipify.org')
+    return response.text
 
 PREFIX_LIST_ID = 'pl-05d04791174282256'
 description = 'Olivier (mobile)'
 ec2 = iblaws.utils.get_service_client(service_name='ec2', region_name='eu-west-2')
-new_ip = '79.168.53.132/32'
+new_ip = f'{get_public_ip()}/32'
 iblaws.utils.ec2_update_managed_prefix_list_item(
     ec2, managed_prefix_list_id=PREFIX_LIST_ID, description=description, cidrip=new_ip
 )
