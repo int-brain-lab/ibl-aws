04644ea503a91f3e5de4ba19890a34e4f08652f6
Olivier Winter <olivier.winter@hotmail.fr>
2026-07-01 18:14:57 +0100

    Update dev IP in both eu-west-2 and us-east-1 prefix lists

--- examples/update_prefix_list_devs.py
+++ examples/update_prefix_list_devs.py
@@ -8,10 +8,16 @@ def get_public_ip():
     return response.text
 
 
-PREFIX_LIST_ID = 'pl-05d04791174282256'
+# "developers" prefix list, mirrored across regions.
+PREFIX_LISTS = {
+    'eu-west-2': 'pl-05d04791174282256',
+    'us-east-1': 'pl-0eb789aa874fc1952',
+}
 description = 'Olivier (mobile)'
-ec2 = iblaws.utils.get_service_client(service_name='ec2', region_name='eu-west-2')
 new_ip = f'{get_public_ip()}/32'
-iblaws.utils.ec2_update_managed_prefix_list_item(
-    ec2, managed_prefix_list_id=PREFIX_LIST_ID, description=description, cidrip=new_ip
-)
+
+for region_name, prefix_list_id in PREFIX_LISTS.items():
+    ec2 = iblaws.utils.get_service_client(service_name='ec2', region_name=region_name)
+    iblaws.utils.ec2_update_managed_prefix_list_item(
+        ec2, managed_prefix_list_id=prefix_list_id, description=description, cidrip=new_ip
+    )
